5e0160f7b76056a30d2ca5405d0dcf4c49e343ce
Atilla Kati <47529551+atillakati@users.noreply.github.com>
2020-10-13 22:08:06 +0200

    Upload examples from 08.10.2020, add PowerPoint and DesignGuidelines into docs folder

---
+++ src/20201008/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld.Beispiel
+{
+	public class MyApp
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hallo zusammen... Hello World!");
+			Console.ReadLine();
+		}
+	}	
+}
---
+++ src/20201008/HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+
+
+            /*
+             * Projektname:   Visitenkarte
+             *       
+             *   Vorname:  Atilla
+             *   Nachname: Kati
+             *   
+             *   Wohnort: Dornbirn    Plz: 6850
+             *   Warum c#:
+             *   
+             *         Weil es soo cool ist!      
+             * 
+             */
+
+            Console.WriteLine("\tVorname:  Atilla\n\tNachname: Kati");            
+            Console.WriteLine("\n\tWohnort: Dornbirn    Plz: 6850\a");
+        }
+    }
+}
