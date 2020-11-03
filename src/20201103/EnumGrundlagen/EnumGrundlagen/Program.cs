009be2a0700fe5658175df5be66f6c93a8ef99b7
Atilla@Wifi <user@WIN10DEV>
2020-11-03 21:32:44 +0100

    Beispiele vom 03.11.2020

---
+++ src/20201103/EnumGrundlagen/EnumGrundlagen/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumGrundlagen
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            //bool participantState = false; //true
+
+            string participantState = "krank";
+            participantState = "anwesend";
+            participantState = "nicht anwesend";
+            participantState = "heute nicht anwesend";
+
+            ParticipantState state = ParticipantState.Unbekannt;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.Clear();
+            Console.WriteLine("Test...");
+
+            if(state == ParticipantState.Krank) 
+            { 
+
+            }
+            
+
+            Console.WriteLine($"State: {state}");
+
+            Console.ResetColor();
+
+            Console.WriteLine();            
+        }
+    }
+}
---
+++ src/20201103/MethodenGL/MethodenGL/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodenGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eineVariable = 12;
+
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Yellow);
+
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Red);
+            DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Green);
+
+            double erg = CalculateWeight(20.0);
+            Console.WriteLine($"Ergebnis: {erg}");            
+        }
+
+        // Signatur
+        //Rückgabetyp Bezeichnung ( [Parameter] )        
+        static void DisplayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        static void DisplayColoredMessage(string message, ConsoleColor messageColor)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = messageColor;
+
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = oldColor;            
+        }
+
+        // Signatur
+        //Rückgabetyp Bezeichnung ( [Parameter] )        
+        static double CalculateWeight(double nettoWeight)
+        {
+            double result = 0.0;
+
+            result = nettoWeight * 1.25;
+
+            return result;
+        }
+
+        static int GetInt(string inputPrompt)
+        {
+
+        }
+    }
+}
