baeeda5fb289140ac7c2533db0ede2319aad7cc8
Atilla Kati <47529551+atillakati@users.noreply.github.com>
2020-11-04 18:34:07 +0100

    create Ausgaben_1

---
+++ src/SolutionsExercises_KW44/DemoSolutionsExercisesKW44/Ausgaben_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ausgaben_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Atilla Kati");
+            Console.WriteLine("Alter: 42");
+            Console.WriteLine("Wohnort:\n");
+            Console.WriteLine("\n\t6850 Dornbirn");
+            Console.WriteLine("Kommentar zu C#:");
+            Console.WriteLine("\n\tC# ist sooooo einfach!");
+        }
+    }
+}
