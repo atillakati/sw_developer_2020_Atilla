e7a3677d4f5cf20dc61b46243b29aff89b819b50
Atilla Kati <47529551+atillakati@users.noreply.github.com>
2020-11-05 19:54:40 +0100

    Update Method Example

--- src/20201103/MethodenGL/MethodenGL/Program.cs
+++ src/20201103/MethodenGL/MethodenGL/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.XPath;
 
 namespace MethodenGL
 {
@@ -23,7 +25,16 @@ namespace MethodenGL
             DisplayColoredMessage("Hallo liebe Leute!", ConsoleColor.Green);
 
             double erg = CalculateWeight(20.0);
-            Console.WriteLine($"Ergebnis: {erg}");            
+            Console.WriteLine($"Ergebnis: {erg}");
+
+            eineVariable = GetInt("Bitte Länge eingeben: ");
+            Console.WriteLine("Länge = " + eineVariable);
+
+            double gewicht = GetDouble("Gewicht eingeben: ");
+            Console.WriteLine($"Das Gewicht ist {gewicht}.");
+
+            DateTime gebDate = GetDateTime("Geburtsdatum eingeben: ");
+            Console.WriteLine($"Sie sind {DateTime.Now.Year - gebDate.Year} Jahre alt.");
         }
 
         // Signatur
@@ -33,6 +44,11 @@ namespace MethodenGL
             Console.WriteLine("Hello!");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="messageColor"></param>
         static void DisplayColoredMessage(string message, ConsoleColor messageColor)
         {
             ConsoleColor oldColor = Console.ForegroundColor;
@@ -40,7 +56,7 @@ namespace MethodenGL
 
             Console.WriteLine(message);
 
-            Console.ForegroundColor = oldColor;            
+            Console.ForegroundColor = oldColor;
         }
 
         // Signatur
@@ -54,9 +70,113 @@ namespace MethodenGL
             return result;
         }
 
+
+        /// <summary>
+        /// Reads an integer value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns>Input value as integer</returns>
         static int GetInt(string inputPrompt)
         {
+            int userInputValue = 0;
+            bool userInputIsValid = false;
 
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to int
+                    userInputValue = int.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
         }
+
+        //==> GetDouble(); GetString(); GetDateTime() => KISS!
+
+        /// <summary>
+        /// Reads an double value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static double GetDouble(string inputPrompt)
+        {
+            double userInputValue = 0;
+            bool userInputIsValid = false;
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to double
+                    userInputValue = double.Parse(Console.ReadLine());
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an DateTime value from console input. The input format must be [dd.MM.yyyy hh:mm:ss].
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static DateTime GetDateTime(string inputPrompt)
+        {
+            DateTime userInputValue = DateTime.MinValue;
+            bool userInputIsValid = false;
+
+            string inputFormat = "dd.MM.yyyy hh:mm:ss";
+
+            do
+            {
+                Console.Write(inputPrompt);
+                try
+                {
+                    //read input value and convert to DateTime
+                    userInputValue = DateTime.ParseExact(Console.ReadLine(), inputFormat, CultureInfo.InvariantCulture);
+                    userInputIsValid = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    userInputIsValid = false;
+                }
+            }
+            while (!userInputIsValid);
+
+            return userInputValue;
+        }
+
+        /// <summary>
+        /// Reads an string value from console input.
+        /// </summary>
+        /// <param name="inputPrompt">Prompt for the user</param>
+        /// <returns></returns>
+        static string GetString(string inputPrompt)
+        {
+            Console.Write(inputPrompt);
+
+            //read input value and convert to string
+            return Console.ReadLine();
+        }
+
     }
 }
