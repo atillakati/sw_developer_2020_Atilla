7554ee74b816bab4cace67f0418cbf2fbbdd9c88
Atilla@Wifi <user@WIN10DEV>
2020-11-17 21:29:25 +0100

    Grundlagen Vererbung

---
+++ src/20201117/GrundlagenVererbung/GrundlagenVererbung/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GrundlagenVererbung
+{
+    public class Employee
+    {
+        private string _vorname;
+        private string _nachname;
+        private Guid _id;
+        private DateTime _geburtsDatum;
+        private decimal _gehalt;
+                
+
+        public Employee(string vorname, string nachname, DateTime geburtsDatum, decimal gehalt)
+        {
+            _vorname = vorname;
+            _nachname = nachname;
+            _geburtsDatum = geburtsDatum;
+            _gehalt = gehalt;
+            _id = Guid.NewGuid();            
+        }
+
+
+        public decimal Gehalt
+        {
+            get { return _gehalt; }        
+        }
+
+        public DateTime GeburtsDatum
+        {
+            get { return _geburtsDatum; }        
+        }
+
+        public Guid Id
+        {
+            get { return _id; }        
+        }
+
+        public string Name
+        {
+            get { return $"{_vorname} {_nachname}"; }
+            set { _nachname = value; }
+        }
+
+
+        public decimal GetCalculatedSalary()
+        {
+            return _gehalt;
+        }
+
+        public string GetInfoString()
+        {
+            return $"[{_id}]: {_vorname} {_nachname} - {_geburtsDatum.Date}";
+        }
+
+    }
+}
