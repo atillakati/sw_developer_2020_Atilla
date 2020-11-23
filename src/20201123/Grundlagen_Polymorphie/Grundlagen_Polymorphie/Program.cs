555d8682748be4cf92d31dbb051fd40907dce3a3
Atilla Kati @ Home <codefreak@kati.bz>
2020-11-23 21:25:29 +0100

    Polymorphie mit virtual/override

---
+++ src/20201123/Grundlagen_Polymorphie/Grundlagen_Polymorphie/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grundlagen_Polymorphie.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grundlagen_Polymorphie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle[] myVehicleList = new Vehicle[]
+            {
+                new Vehicle(),
+                new Vehicle("Badmobil", "Black Edition Super v2", 250),
+                new Plane("F16", "Euro-Fighter Legal Edition", 2500, 20000.0),
+                new Car("VW", "Rabbit Golf", 120)
+            };
+
+            ////Hauptprogramm
+            //foreach (var vehicle in myVehicleList)
+            //{
+            //    Display(vehicle);
+            //}
+
+            Console.WriteLine(myVehicleList[3]);
+        }
+
+        private static void Display(Vehicle vehicle)
+        {            
+            Console.WriteLine(vehicle.GetInfoString());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/20201123/Grundlagen_Polymorphie/Grundlagen_Polymorphie/Types/Radio.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grundlagen_Polymorphie.Types
+{
+    public class Radio
+    {
+        private double _frequency;
+        private int _volume;
+        private Power _powerState;
+
+        public Radio()
+        {
+            _frequency = 90.5;
+            _volume = 3;
+            _powerState = Power.Off;
+        }
+
+        public Power PowerState
+        {
+            get { return _powerState; }
+        }
+
+        public int Volume
+        {
+            get { return _volume; }
+            set
+            {
+                //ToDo: Limit possible values between 0-10!
+                _volume = value;
+            }
+        }
+
+        public double Frequency
+        {
+            get { return _frequency; }
+            set
+            {
+                //ToDo: Limit possible values between 86.0 - 102.0 (FM Band)
+                _frequency = value;
+            }
+        }
+
+
+        public string GetInfoString()
+        {
+            return $"State: {_powerState} | Freq: {_frequency} MHz | Volume: {_volume}";
+        }
+
+        public void SetPowerState(Power newPowerState)
+        {
+            _powerState = newPowerState;
+
+            //ToDo: Macht euch Gedanken darüber, welche Status wann übernommen werden dürfen. => Umsetzen!
+
+            switch (newPowerState)
+            {
+                case Power.On:
+                    Console.WriteLine("Radio macht nun Musik: " + GetInfoString());
+                    break;
+                case Power.Off:
+                    Console.WriteLine("Radio ist aus.");
+                    break;
+                case Power.Suspend:
+                    Console.WriteLine("Bis bald: " + GetInfoString());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
