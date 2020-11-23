b1eab2b15ab542145c3899f66b194e13feedd964
Atilla Kati @ Home <codefreak@kati.bz>
2020-11-23 21:25:05 +0100

    Vererbung HAT-EIN , Container/Delegate Modell

---
+++ src/20201123/Grundlagen_HatEin_Vererbung/Grundlagen_HatEin_Vererbung/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grundlagen_HatEin_Vererbung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle myCar = new Vehicle("Badmobil", "Badmobil Black Edition V2", 250);
+
+            Console.WriteLine(myCar.GetInfoString());
+            //myCar.VehicleRadio.SetPowerState(Power.On);
+
+            //myCar.VehicleRadio.SetPowerState(Power.Off);
+            //myCar.VehicleRadio.Volume = 5;
+
+            myCar.State = VehicleState.Unlocked;
+
+            Console.WriteLine();
+            Console.WriteLine(myCar.GetInfoString());
+
+            myCar.ChangeRadioPowerState(Power.On);
+
+            Console.WriteLine();            
+            Console.WriteLine();
+
+            myCar.MediaVolume = 8;
+
+            Console.WriteLine(myCar.GetInfoString());
+
+            
+        }
+    }
+}
