9b6ccbf45a94c8b6652b57d042d0e3771d31b4da
Atilla Kati @ Home <codefreak@kati.bz>
2020-11-24 19:54:03 +0100

    Abstrakte Klassen

---
+++ src/20201124/AbstrakteKlassen/AbstrakteKlassen/DataRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrakteKlassen
+{
+    public abstract class DataRepository
+    {
+        private int _maxSize;
+        private string _name;
+
+        public DataRepository(string name, int maxSize)
+        {
+            _name = name;
+            _maxSize = maxSize;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int MaxSize
+        {
+            get { return _maxSize; }
+        }
+
+        public abstract void Write(string data);
+
+        public abstract string Read();
+
+        public override string ToString()
+        {
+            return $"RepositoryType: {_name} | Size limit: {_maxSize}";
+        }
+
+    }
+}
---
+++ src/20201124/AbstrakteKlassen/AbstrakteKlassen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrakteKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataRepository repository = new MemoryRepository(200);
+            //DataRepository repository = new TextFileRepository("myData.txt", 200);
+
+            //daten speichern
+            PersistMyData(repository, "Hallo Welt! Dies ist eine Testdatensatz. ");
+
+            //daten lesen
+            string myData = ReadMyData(repository);
+
+            //ausgabe
+            Console.WriteLine(myData);
+        }
+
+        static string ReadMyData(DataRepository repository)
+        {
+            if(repository != null)
+            {
+                Console.WriteLine(repository);
+                return repository.Read();
+            }
+
+            return string.Empty;
+        }
+
+        static void PersistMyData(DataRepository repository, string dataToPersist)
+        {
+            if(repository != null)
+            {
+                Console.WriteLine(repository);
+                repository.Write(dataToPersist);
+            }
+        }
+    }
+}
