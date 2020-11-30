bffc07e4a9f270ffc6ba587c2cc54d49f192c4a3
Atilla Kati @ Home <codefreak@kati.bz>
2020-11-30 20:55:22 +0100

    Exceptions Advanced

---
+++ src/20201130/ExceptionsAdvanced/ExceptionsAdvanced/CalculationImpossibleException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAdvanced
+{
+    public class CalculationImpossibleException : ArgumentException
+    {
+        private string _invalidParameter;
+
+        public CalculationImpossibleException(string invalidParameter)
+            : base()
+        {
+            _invalidParameter = invalidParameter;
+        }
+
+        public override string Message
+        {
+            get 
+            {
+                return $"Parameter '{_invalidParameter}' hat einen ungültigen Inhalt.";
+            }
+        }
+    }
+}
---
+++ src/20201130/ExceptionsAdvanced/ExceptionsAdvanced/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testClass = new TestClass();
+
+            try
+            {
+                testClass.Calculate(0, 2);
+            }
+            catch (CalculationImpossibleException cex)
+            {
+                Console.WriteLine("Eine CalculationImpossibleException wurde abgefangen:");
+                Console.WriteLine($"ERROR: {cex.Message}");
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine("Juhuuu!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+    }
+}
