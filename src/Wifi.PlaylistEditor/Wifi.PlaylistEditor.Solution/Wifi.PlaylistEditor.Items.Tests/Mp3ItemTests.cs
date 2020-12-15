780b37c66264fada32fbb9f0627f49d8e2c6643b
Atilla@Wifi <user@WIN10DEV>
2020-12-15 20:13:57 +0100

    Complete mp3Item tests

--- src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Items.Tests/Mp3ItemTests.cs
+++ src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Items.Tests/Mp3ItemTests.cs
@@ -14,11 +14,16 @@ namespace Wifi.PlaylistEditor.Items.Tests
         private IPlaylistItem _fixture;
 
 
+        [SetUp]
+        public void TestInit()
+        {
+            _fixture = new Mp3Item(@"C:\Users\user\Music\TestMediaFiles\001 - Bruno Mars - Grenade.mp3");
+        }
+
         [Test]
         public void GetTitle()
         {
-            //arrange
-            _fixture = new Mp3Item(@"C:\Users\user\Music\TestMediaFiles\001 - Bruno Mars - Grenade.mp3");
+            //arrange            
 
             //act
             var title = _fixture.Title;
@@ -32,13 +37,68 @@ namespace Wifi.PlaylistEditor.Items.Tests
         public void GetDuration()
         {
             //arrange
-            _fixture = new Mp3Item(@"C:\Users\user\Music\TestMediaFiles\001 - Bruno Mars - Grenade.mp3");
 
             //act
             var duration = _fixture.Duration;
 
             //assert            
             Assert.That(duration, Is.EqualTo(TimeSpan.FromSeconds(222.188)));
+        }
+
+        [Test]
+        public void GetArtist()
+        {
+            //arrange
+
+            //act
+            var artist = _fixture.Artist;
+
+            //assert            
+            Assert.That(artist, Is.EqualTo("Bruno Mars"));
+        }
+
+        [Test]
+        public void GetPath()
+        {
+            var demoMp3Path = @"C:\Users\user\Music\TestMediaFiles\001 - Bruno Mars - Grenade.mp3";
+
+            //arrange
+
+            //act
+            var path = _fixture.Path;
+
+            //assert            
+            Assert.That(path, Is.EqualTo(demoMp3Path));
+        }
+
+
+        [Test]
+        public void GetThumbnail()
+        {
+            var demoMp3Path = @"C:\Users\user\Music\TestMediaFiles\001 - Bruno Mars - Grenade.mp3";
+
+            //arrange
+
+            //act
+            var thumbnail = _fixture.Thumbnail;
+
+            //assert            
+            Assert.That(thumbnail, Is.Not.Null);            
+        }
+
+        [Test]
+        public void GetThumbnail_WithMp3FileWithNoImage()
+        {
+            var demoMp3Path = @"C:\Users\user\Music\TestMediaFiles\002 - Lena - Taken By A Stranger.mp3";
+             
+            //arrange
+            _fixture = new Mp3Item(demoMp3Path);
+
+            //act
+            var thumbnail = _fixture.Thumbnail;
+
+            //assert            
+            Assert.That(thumbnail, Is.Null);
         }
 
         [Test]
@@ -58,7 +118,7 @@ namespace Wifi.PlaylistEditor.Items.Tests
         public void GetTitle_FilePathIsEmpty()
         {
             //arrange
-            var fixture = new Mp3Item(null);
+            var fixture = new Mp3Item(string.Empty);
 
             //act
             var title = fixture.Title;
