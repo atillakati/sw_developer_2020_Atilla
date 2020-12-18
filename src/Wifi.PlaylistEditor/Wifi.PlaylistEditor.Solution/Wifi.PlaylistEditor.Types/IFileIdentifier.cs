d6d900f7415b0c9a685d77e9d5ca2a83ccec2527
Atilla Kati @ Home <codefreak@kati.bz>
2020-12-18 08:58:24 +0100

    Add interface documentation

--- src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Types/IFileIdentifier.cs
+++ src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Types/IFileIdentifier.cs
@@ -1,8 +1,18 @@
 ﻿namespace Wifi.PlaylistEditor.Types
 {
+    /// <summary>
+    /// Interface to describe file types and their default extension
+    /// </summary>
     public interface IFileIdentifier
     {
+        /// <summary>
+        /// The default extension of the file. eg: .mp3 or .ogg
+        /// </summary>
         string Extension { get; }
+
+        /// <summary>
+        /// A short prosa description of the file type
+        /// </summary>
         string Description { get; }
     }
 }
--- src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Types/IRepository.cs
+++ src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Types/IRepository.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Wifi.PlaylistEditor.Types
 {
+    /// <summary>
+    /// Interface to implement classes to load and save a IPlaylist document into a file
+    /// </summary>
     public interface IRepository : IFileIdentifier
     {
+        /// <summary>
+        /// Writes the provided playlist data into the file in filePath
+        /// </summary>
+        /// <param name="filePath">The file to create</param>
+        /// <param name="playlist">Playlist data to write into a file</param>
         void Save(string filePath, IPlaylist playlist);
 
+        /// <summary>
+        /// Loads the playlist data from provided file
+        /// </summary>
+        /// <param name="filePath">Filepath to load</param>
+        /// <returns></returns>
         IPlaylist Load(string filePath);
     }
 }
