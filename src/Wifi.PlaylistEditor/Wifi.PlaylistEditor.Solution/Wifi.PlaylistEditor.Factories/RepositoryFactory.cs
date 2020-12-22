7cbe04fef95304c39f7d8aa297cdbe029d74f179
Atilla @ WIFI <Atilla@DESKTOP-85T0EU9>
2020-12-22 20:03:49 +0100

    Implement and define IRepository factory

---
+++ src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Factories/RepositoryFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.PlaylistEditor.Repositories;
+using Wifi.PlaylistEditor.Types;
+
+namespace Wifi.PlaylistEditor.Factories
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        private readonly IPlaylistItemFactory _playlistItemFactory;
+
+        public RepositoryFactory(IPlaylistItemFactory playlistItemFactory)
+        {
+            _playlistItemFactory = playlistItemFactory;
+        }
+
+        public IEnumerable<IFileIdentifier> KnownTypes
+        {
+            get
+            {
+                return new IFileIdentifier[]
+                {
+                    new M3uRepository(_playlistItemFactory),
+                    new WplRepository(_playlistItemFactory)
+                };
+            }
+        }
+
+        public IRepository Create(string playlistPath)
+        {
+            IRepository repository = null;
+
+            if (string.IsNullOrEmpty(playlistPath))
+            {
+                return null;
+            }
+
+            var extension = Path.GetExtension(playlistPath);
+
+            switch (extension)
+            {
+                case ".m3u":
+                    repository = new M3uRepository(_playlistItemFactory);
+                    break;
+
+                case ".wpl":
+                    repository = new WplRepository(_playlistItemFactory);
+                    break;
+
+                default:
+                    repository = null;
+                    break;
+            }
+
+            return repository;
+        }
+    }
+}
---
+++ src/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Solution/Wifi.PlaylistEditor.Types/IRepositoryFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlaylistEditor.Types
+{
+    public interface IRepositoryFactory
+    {
+        IEnumerable<IFileIdentifier> KnownTypes { get; }
+
+        IRepository Create(string playlistPath);
+    }
+}
